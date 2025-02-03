73e641898d37abbfa61b0bcde01ecfd03b17c9a5
chanyutl <chanyutx10@gmail.com>
2025-02-03 15:41:34 +0700

    Add whitelist management and update purchase limits in IAIPresaleV2 contract

--- contracts/IAIPresaleV2.sol
+++ contracts/IAIPresaleV2.sol
@@ -18,13 +18,18 @@ contract IAIPresaleV2 is Ownable, ReentrancyGuard, Pausable {
     uint256 public totalTokensSold;
     uint256 public maxSaleAmount;
 
-    uint256 public minPurchaseAmount; // 0.1
-    uint256 public maxPurchaseAmount;
+    uint256 public minPurchaseAmount; // would be 0.1
 
     // wallet address who will receive the revenue from token selling
     address public revenueReceiver;
 
     mapping(address => uint256) public userTotalPurchased;
+
+    mapping(address => bool) public whitelisted;
+
+    mapping(address => uint256) public whitelistMaxAmount;
+
+    address[] private whitelistedAddresses;
 
     event TokensPurchased(address indexed buyer, uint256 amount, uint256 cost);
     event PresaleConfigUpdated(
@@ -32,11 +37,24 @@ contract IAIPresaleV2 is Ownable, ReentrancyGuard, Pausable {
         uint256 newStartTime,
         uint256 newEndTime
     );
+
     event PurchaseLimitsUpdated(uint256 minAmount, uint256 maxAmount);
     event MaxSaleAmountUpdated(uint256 amount);
     event USDTWithdrawn(address to, uint256 amount);
     event PresaleTokensWithdrawn(address to, uint256 amount);
     event RevenueReceiverUpdated(address newReceiver);
+
+    event AddressWhitelisted(address indexed account, uint256 maxAmount);
+    event AddressRemovedFromWhitelist(address indexed account);
+    event BatchWhitelistAdded(address[] accounts, uint256[] maxAmounts);
+    event BatchWhitelistRemoved(address[] accounts);
+    event MinPurchaseLimitUpdated(uint256 minAmount);
+    event BatchWhitelistMaxAmountUpdated(
+        address[] accounts,
+        uint256[] newMaxAmounts
+    );
+
+    event PrematureTokenWithdrawal(address to, uint256 amount);
 
     constructor(
         address _usdtToken,
@@ -46,8 +64,7 @@ contract IAIPresaleV2 is Ownable, ReentrancyGuard, Pausable {
         uint256 _startTime,
         uint256 _endTime,
         uint256 _maxSaleAmount,
-        uint256 _minPurchaseAmount,
-        uint256 _maxPurchaseAmount
+        uint256 _minPurchaseAmount
     ) Ownable(msg.sender) {
         require(
             _usdtToken != address(0) && _iaiPresaleToken != address(0),
@@ -67,7 +84,6 @@ contract IAIPresaleV2 is Ownable, ReentrancyGuard, Pausable {
         maxSaleAmount = _maxSaleAmount;
 
         minPurchaseAmount = _minPurchaseAmount; // for example: 100 * 1e18 => 100 tokens minimum
-        maxPurchaseAmount = _maxPurchaseAmount; // for example: 10000 * 1e18 => 10000 tokens maximum
     }
 
     modifier isSaleActive() {
@@ -79,6 +95,7 @@ contract IAIPresaleV2 is Ownable, ReentrancyGuard, Pausable {
     function buyTokens(
         uint256 usdtAmount
     ) external nonReentrant isSaleActive whenNotPaused {
+        require(whitelisted[msg.sender], "Address not whitelisted");
         require(revenueReceiver != address(0), "Invalid revenue receiver");
         require(usdtAmount > 0, "Amount must be greater than 0");
 
@@ -89,14 +106,15 @@ contract IAIPresaleV2 is Ownable, ReentrancyGuard, Pausable {
             tokenAmount >= minPurchaseAmount,
             "Below minimum purchase amount"
         );
-        require(
-            tokenAmount <= maxPurchaseAmount,
-            "Exceeds maximum purchase amount"
-        );
-        require(
-            userTotalPurchased[msg.sender] + tokenAmount <= maxPurchaseAmount,
-            "Exceeds user maximum"
-        );
+
+        uint256 maxAmount = whitelistMaxAmount[msg.sender];
+        if (maxAmount > 0) {
+            // if maxAmount is 0, there is no limit
+            require(
+                userTotalPurchased[msg.sender] + tokenAmount <= maxAmount,
+                "Exceeds total allowed purchase amount"
+            );
+        }
 
         require(
             totalTokensSold + tokenAmount <= maxSaleAmount,
@@ -146,14 +164,19 @@ contract IAIPresaleV2 is Ownable, ReentrancyGuard, Pausable {
         emit PresaleConfigUpdated(newPrice, newStartTime, newEndTime);
     }
 
-    function setPurchaseLimits(
-        uint256 _minAmount,
-        uint256 _maxAmount
-    ) external onlyOwner {
-        require(_minAmount > 0 && _maxAmount > _minAmount, "Invalid limits");
+    function setMinPurchaseAmount(uint256 _minAmount) external onlyOwner {
+        require(_minAmount > 0, "Invalid minimum");
         minPurchaseAmount = _minAmount;
-        maxPurchaseAmount = _maxAmount;
-        emit PurchaseLimitsUpdated(_minAmount, _maxAmount);
+        emit MinPurchaseLimitUpdated(_minAmount);
+    }
+
+    function setMaxSaleAmount(uint256 _maxSaleAmount) external onlyOwner {
+        require(
+            _maxSaleAmount > totalTokensSold,
+            "Max sale amount cannot be less than already sold amount"
+        );
+        maxSaleAmount = _maxSaleAmount;
+        emit MaxSaleAmountUpdated(_maxSaleAmount);
     }
 
     function pause() external onlyOwner whenNotPaused {
@@ -197,15 +220,6 @@ contract IAIPresaleV2 is Ownable, ReentrancyGuard, Pausable {
         emit PresaleTokensWithdrawn(owner(), balance);
     }
 
-    function setMaxSaleAmount(uint256 _maxSaleAmount) external onlyOwner {
-        require(
-            _maxSaleAmount > totalTokensSold,
-            "Max sale amount cannot be less than already sold amount"
-        );
-        maxSaleAmount = _maxSaleAmount;
-        emit MaxSaleAmountUpdated(_maxSaleAmount);
-    }
-
     function getAvailableSellAmount() public view returns (uint256) {
         return maxSaleAmount - totalTokensSold;
     }
@@ -214,5 +228,163 @@ contract IAIPresaleV2 is Ownable, ReentrancyGuard, Pausable {
         require(_revenueReceiver != address(0), "Invalid address");
         revenueReceiver = _revenueReceiver;
         emit RevenueReceiverUpdated(_revenueReceiver);
+    }
+
+    // Add new functions for whitelist management
+    function addToWhitelist(
+        address account,
+        uint256 maxAmount // 0 means no limit
+    ) external onlyOwner {
+        require(account != address(0), "Invalid address");
+        require(!whitelisted[account], "Address already whitelisted");
+
+        whitelisted[account] = true;
+        whitelistMaxAmount[account] = maxAmount;
+        whitelistedAddresses.push(account);
+        emit AddressWhitelisted(account, maxAmount);
+    }
+
+    function removeFromWhitelist(address account) external onlyOwner {
+        require(whitelisted[account], "Address not whitelisted");
+        whitelisted[account] = false;
+
+        // Remove from array by finding and replacing with last element
+        for (uint256 i = 0; i < whitelistedAddresses.length; i++) {
+            if (whitelistedAddresses[i] == account) {
+                whitelistedAddresses[i] = whitelistedAddresses[
+                    whitelistedAddresses.length - 1
+                ];
+                whitelistedAddresses.pop();
+                break;
+            }
+        }
+
+        emit AddressRemovedFromWhitelist(account);
+    }
+
+    function batchAddToWhitelist(
+        address[] calldata accounts,
+        uint256[] calldata maxAmounts // 0 means no limit
+    ) external onlyOwner {
+        require(accounts.length == maxAmounts.length, "Arrays length mismatch");
+
+        for (uint256 i = 0; i < accounts.length; i++) {
+            require(accounts[i] != address(0), "Invalid address");
+            whitelisted[accounts[i]] = true;
+            whitelistMaxAmount[accounts[i]] = maxAmounts[i];
+        }
+        emit BatchWhitelistAdded(accounts, maxAmounts);
+    }
+
+    function batchRemoveFromWhitelist(
+        address[] calldata accounts
+    ) external onlyOwner {
+        for (uint256 i = 0; i < accounts.length; i++) {
+            if (whitelisted[accounts[i]]) {
+                whitelisted[accounts[i]] = false;
+            }
+        }
+        emit BatchWhitelistRemoved(accounts);
+    }
+
+    function updateWhitelistMaxAmount(
+        address account,
+        uint256 newMaxAmount // 0 means no limit
+    ) external onlyOwner {
+        require(whitelisted[account], "Address not whitelisted");
+        if (newMaxAmount > 0) {
+            require(
+                newMaxAmount >= userTotalPurchased[account],
+                "New max amount below already purchased amount"
+            );
+        }
+
+        whitelistMaxAmount[account] = newMaxAmount;
+        emit AddressWhitelisted(account, newMaxAmount);
+    }
+
+    function batchUpdateWhitelistMaxAmount(
+        address[] calldata accounts,
+        uint256[] calldata newMaxAmounts
+    ) external onlyOwner {
+        require(
+            accounts.length == newMaxAmounts.length,
+            "Arrays length mismatch"
+        );
+
+        for (uint256 i = 0; i < accounts.length; i++) {
+            require(whitelisted[accounts[i]], "Address not whitelisted");
+            if (newMaxAmounts[i] > 0) {
+                require(
+                    newMaxAmounts[i] >= userTotalPurchased[accounts[i]],
+                    "New max amount below already purchased amount"
+                );
+            }
+            whitelistMaxAmount[accounts[i]] = newMaxAmounts[i];
+        }
+        emit BatchWhitelistMaxAmountUpdated(accounts, newMaxAmounts);
+    }
+
+    function isWhitelisted(address account) external view returns (bool) {
+        return whitelisted[account];
+    }
+
+    // Add new function for pagination
+    function getWhitelistedAddresses(
+        uint256 offset,
+        uint256 limit
+    )
+        external
+        view
+        returns (
+            address[] memory addresses,
+            uint256[] memory maxAmounts,
+            uint256 total
+        )
+    {
+        uint256 totalAddresses = whitelistedAddresses.length;
+
+        if (offset >= totalAddresses) {
+            return (new address[](0), new uint256[](0), totalAddresses);
+        }
+
+        uint256 size = totalAddresses - offset;
+        if (size > limit) {
+            size = limit;
+        }
+
+        addresses = new address[](size);
+        maxAmounts = new uint256[](size);
+
+        for (uint256 i = 0; i < size; i++) {
+            address account = whitelistedAddresses[offset + i];
+            addresses[i] = account;
+            maxAmounts[i] = whitelistMaxAmount[account];
+        }
+
+        return (addresses, maxAmounts, totalAddresses);
+    }
+
+    function getTotalWhitelistedAddresses() external view returns (uint256) {
+        return whitelistedAddresses.length;
+    }
+
+    // prematureWithdrawPresaleTokens function will be used to withdraw unsold tokens anytime
+    // it is different from withdrawUnsoldPresaleTokens which can be called only after presale ends
+    function prematureWithdrawPresaleTokens(
+        uint256 amount
+    ) external onlyOwner whenPaused {
+        require(amount > 0, "Amount must be greater than 0");
+        require(
+            iaiPresaleToken.balanceOf(address(this)) >= amount,
+            "Insufficient token balance"
+        );
+
+        require(
+            iaiPresaleToken.transfer(owner(), amount),
+            "Token transfer failed"
+        );
+
+        emit PrematureTokenWithdrawal(owner(), amount);
     }
 }
